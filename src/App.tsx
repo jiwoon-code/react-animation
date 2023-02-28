3243902426bc4dc4967d84c2d5f3181455ded41a
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2023-02-28 22:38:32 +0900

    variation commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
@@ -8,7 +9,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
@@ -16,10 +17,22 @@ const Box = styled.div`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: { scale: 0 },
+  end: {
+    scale: 1,
+    rotate: 360,
+    transition: { type: "spring", bounce: 0.35, delay: 0.5 },
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   );
 }
