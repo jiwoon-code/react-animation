c16e78b1247101f2bcde335be3bf570395cd7553
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2023-02-28 23:36:53 +0900

    variation part 2

--- src/App.tsx
+++ src/App.tsx
@@ -12,27 +12,55 @@ const Wrapper = styled(motion.div)`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+  background-color: rgba(255, 255, 255, 0);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+const Circle = styled(motion.div)`
   background-color: white;
-  border-radius: 10px;
+  height: 70px;
+  width: 70px;
+  border-radius: 35px;
+  place-self: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
+const boxVariants = {
+  start: { opacity: 0, scale: 0 },
   end: {
     scale: 1,
-    rotate: 360,
-    transition: { type: "spring", bounce: 0.35, delay: 0.5 },
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.9,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariations = {
+  start: {
+    opacity: 0,
+  },
+  end: {
+    opacity: 1,
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
-      <Box variants={myVars} initial="start" animate="end" />
-      <Box variants={myVars} initial="start" animate="end" />
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariations} />
+        <Circle variants={circleVariations} />
+        <Circle variants={circleVariations} />
+        <Circle variants={circleVariations} />
+      </Box>
     </Wrapper>
   );
 }
